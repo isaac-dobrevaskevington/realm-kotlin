1a3b1b2f57e0982a5f00c65233dce9a1bc18b073
isaac-dobrevaskevington <isaac.dobrevaskevington@t4connex.com>
2025-02-06 12:11:40 +0000

    Update publish_release.sh

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -24,7 +24,7 @@ set -e
 
 HERE=$(dirname `realpath "$0"`)
 REALM_KOTLIN_PATH="$HERE/.."
-RELEASE_VERSION=""
+RELEASE_VERSION="3.2.1"
 MAVEN_CENTRAL_USER="$1"
 MAVEN_CENTRAL_KEY="$2"
 GRADLE_PORTAL_KEY="$3"
@@ -34,57 +34,6 @@ GRADLE_BUILD_PARAMS="${5}"
 abort_release() {
   # Reporting failures to #realm-java-team-ci is done from Jenkins
   exit 1
-}
-
-check_env() {
-  echo "Checking environment..."
-
-  # Try to find s3cmd
-  path_to_s3cmd=$(type s3cmd)
-  if [ -x "$path_to_s3cmd" ]
-  then
-    echo "Cannot find executable file 's3cmd'. Aborting."
-    abort_release
-  fi
-
-  # Try to find git
-  path_to_git=$(type git)
-  if [ -x "$path_to_git" ]
-  then
-    echo "Cannot find executable file 'git'. Aborting."
-    abort_release
-  fi
-
-  echo "Environment is OK."
-}
-
-verify_release_preconditions() {
-  echo "Checking release branch..."
-  git fetch --tags
-  gitTag=`git describe --tags | tr -d '[:space:]'`
-  version=`grep "const val version" buildSrc/src/main/kotlin/Config.kt | cut -d \" -f2`
-
-  if [ "v$version" = "$gitTag" ]
-  then
-    RELEASE_VERSION=$version
-    echo "Git tag and version.txt matches: $version. Continue releasing."
-  else
-    echo "Version in Config.kt was '$version' while the branch was tagged with '$gitTag'. Aborting release."
-    abort_release
-  fi
-}
-
-verify_changelog() {
-  echo "Checking CHANGELOG.md..."
-  query="grep -c '^## $RELEASE_VERSION ([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])' $REALM_KOTLIN_PATH/CHANGELOG.md"
-
-  if [ `eval $query` -ne 1 ]
-  then
-    echo "Changelog does not appear to be correct. First line should match the version being released and the date should be set. Aborting."
-    abort_release
-  else
-    echo "CHANGELOG date and version is correctly set."
-  fi
 }
 
 create_javadoc() {
@@ -110,10 +59,6 @@ publish_artifacts() {
 ######################################
 # Run Release steps
 ######################################\
-
-check_env
-verify_release_preconditions
-verify_changelog
 
 if [ "$1" != "verify" ]; then
   create_javadoc
